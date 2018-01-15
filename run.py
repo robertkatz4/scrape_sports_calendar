37c17de85a71638bff72758e702efb2e8ffacf5b
robertkatz4 <robert.katz@successacademies.org>
2018-01-15 13:24:11 -0500

    remove print statement

--- run.py
+++ run.py
@@ -6,6 +6,5 @@ from gcal_nba_ntv import sports_delete_events
 
 sports_cal_id = 'vkuae4kj45qoo09m53l9sua280@group.calendar.google.com'
 nba_list = nba_ntl_17_18()
-print(nba_list[1])
 sports_delete_events('vkuae4kj45qoo09m53l9sua280@group.calendar.google.com', nba_list[0])
 sports_insert_matches(sports_cal_id, nba_list[1])
