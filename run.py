a318450c59bfbe950bff0a04ebc40093bf7821ac
Robert Katz <robert.katz@successacademies.org>
2019-10-27 10:45:07 -0400

    implement gin config

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 
+import gin
+
 from nba17_18 import nba_ntl_17_18
 from nba17_18 import knicks_17_18
 from gcal_nba_ntv import sports_insert_matches
 from gcal_nba_ntv import sports_delete_events
-
-sports_cal_id = 'vkuae4kj45qoo09m53l9sua280@group.calendar.google.com'
-nba_ntl_cal_id = 'snoof255d4acqjlhh9aogk3l5o@group.calendar.google.com'
 
 schedule_begin_end = {}
 schedule_begin_end['nba'] = {}
@@ -22,13 +21,20 @@ schedule_begin_end['nba']['matches'] = nba_ntl_17_18()
 #         datetime(2018, 04, 12, 12, 00, 00).isoformat() + 'Z'
 # schedule_begin_end['nyk']['matches'] = knicks_17_18()
 
-cal_list = [nba_ntl_cal_id]
+@gin.configurable
+def main(cal=None):
+    cal=cal
+    cal_list = [cal]
+    for i in cal_list:
+        for schedule_name, schedule_values in schedule_begin_end.iteritems():
+            sports_delete_events(i,
+                                 schedule_name,
+                                 schedule_values)
+            print('Inserting ' + str(len(schedule_values['matches'])) +
+                  ' matches from ' + schedule_name + ' schedule')
+            sports_insert_matches(i, schedule_values['matches'])
 
-for i in cal_list:
-    for schedule_name, schedule_values in schedule_begin_end.iteritems():
-        sports_delete_events(i,
-                             schedule_name,
-                             schedule_values)
-        print('Inserting ' + str(len(schedule_values['matches'])) +
-              ' matches from ' + schedule_name + ' schedule')
-        sports_insert_matches(i, schedule_values['matches'])
+
+if __name__ == "__main__":
+    gin.parse_config_file('top_level_params.gin')
+    main()
